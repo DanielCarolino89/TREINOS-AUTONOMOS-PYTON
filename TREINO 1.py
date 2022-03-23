b390fd90bb05bc7c5919a52b099932bcfa212568
Daniel de Godoy Carolino <99697178+DanielCarolino89@users.noreply.github.com>
2022-03-22 21:25:37 -0300

    TREINOS AUTONOMOS PYTON
    TREINOS AUTONOMOS PYTON

---
+++ TREINO 1.py
@@ -0,0 +1,17 @@
+
+
+nome = input(" Qual seu nome? ")
+print('Olá, ', nome)
+
+alt = input(" Qual sua altura? ")
+year = input(" Qual sua idade? ")
+
+print(" Seu nome é" ,nome ," tem " , year, " de idade e ", alt," altura ")
+
+N1 = int(input(" DIGITE NUME DE 0 A 10 "))
+N2 = int(input(" DIGITE NUME DE 0 A 10 "))
+N3 = N1 * N2
+N4 = N1 + N2
+print(" MULTIPLICAÇAO ", N3, " E A SOMA É ", N4 )
+
+
